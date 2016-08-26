b8ef5c73fc1aee0d9cc1fd3eea3a4c1056a57f7f
KrunaL Parvadiya <krunalcp@gmail.com>
2016-08-26 20:08:44 +0530

    Permitted Parameters Changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,8 @@ class ApplicationController < ActionController::Base
 
   private
    def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit( :email, :first_name,:last_name,:user_type_id,:mobile_number, :password, :password_confirmation, :remember_me) }
-    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit( :email, :password, :remember_me) }
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit( :email, :first_name,:last_name,:user_type_id, :mobile_number, :password, :password_confirmation, :current_password) }
+    devise_parameter_sanitizer.for(:sign_up) << [:email, :first_name,:last_name,:user_type_id,:mobile_number, :password, :password_confirmation, :remember_me]
+    devise_parameter_sanitizer.for(:sign_in) << [:email, :password, :remember_me]
+    devise_parameter_sanitizer.for(:account_update) << [:email, :first_name,:last_name,:user_type_id, :mobile_number, :password, :password_confirmation, :current_passworde]
   end
 end
